a3dcb616c19ed9ebd93aa3965ceb00cc561aba03
adriswara <chevellerz454@yahoo.com>
2024-08-08 21:21:59 +0700

    carousel finished

--- index.php
+++ index.php
@@ -9,29 +9,27 @@
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
             aria-label="Slide 3"></button>
     </div>
+    <!--  -->
     <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="img/chicago.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
+        <!--  -->
+        <?php
+        include "connection.php";
+        $queryCarousel = "SELECT * FROM `artikel` ORDER BY id_artikel DESC LIMIT 3";
+        $dataCarousel = mysqli_query($db_connection, $queryCarousel);
+        $i = 1;
+        foreach ($dataCarousel as $data):
+            ?>
+            <div class="carousel-item active">
+                <img src="img/<?= $data['display_artikel']; ?>" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5><?php echo $data['topik_artikel'] ?></h5>
+                    <p><?php echo $data['judul_artikel'] ?></p>
+                </div>
             </div>
-        </div>
-        <div class="carousel-item">
-            <img src="img/la.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img src="img/ny.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>Some representative placeholder content for the third slide.</p>
-            </div>
-        </div>
+            <!--  -->
+        <?php endforeach ?>
     </div>
+    <!--  -->
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
