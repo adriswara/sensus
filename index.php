ec96786426f46b72e5617dd1d186a651e066f3ef
adriswara <chevellerz454@yahoo.com>
2024-08-08 22:38:15 +0700

    printed all rw population

--- index.php
+++ index.php
@@ -55,15 +55,17 @@
         <!--  -->
         <div class="col">
             <div class="card shadow-sm">
-                <a href="read_artikel.php"> <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="img/<?= $data['display_artikel']; ?>" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <a href="read_artikel.php"> <img class="bd-placeholder-img card-img-top" width="100%" height="225"
+                        src="img/<?= $data['display_artikel']; ?>" preserveAspectRatio="xMidYMid slice" focusable="false">
                     <rect width="100%" height="100%" fill="#55595c" /></img>
                 </a>
                 <div class="card-body">
-                    <p class="card-text"> <?php echo $data['topik_artikel'] ?> <?php echo $data['judul_artikel'] ?> </p>
+                    <p class="card-text"> <?php echo $data['topik_artikel'] ?>     <?php echo $data['judul_artikel'] ?> </p>
                     <p class="card-text"> <?php echo $data['isi_artikel'] ?></p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <a href="read_artikel.php"><button type="button" class="btn btn-sm btn-outline-secondary"> Baca </button></a>
+                            <a href="read_artikel.php"><button type="button" class="btn btn-sm btn-outline-secondary"> Baca
+                                </button></a>
                         </div>
                     </div>
                 </div>
@@ -73,7 +75,7 @@
     <?php endforeach ?>
 
     <!--  -->
-  
+
 </div>
 
 <nav class="bg-gray-800">
@@ -183,14 +185,166 @@
 </div>
 
 
+<?php
+include "connection.php";
+// RW1
+$queryRW1 = " select count(id_warga) as totalRW1 from warga where rw_warga = 1; ";
+$dataRW1 = mysqli_query($db_connection, $queryRW1);
+// RW2
+$queryRW2 = " select count(id_warga) as totalRW2 from warga where rw_warga = 2; ";
+$dataRW2 = mysqli_query($db_connection, $queryRW2);
+// RW3
+$queryRW3 = " select count(id_warga) as totalRW3 from warga where rw_warga = 3; ";
+$dataRW3 = mysqli_query($db_connection, $queryRW3);
+// RW4
+$queryRW4 = " select count(id_warga) as totalRW4 from warga where rw_warga = 4; ";
+$dataRW4 = mysqli_query($db_connection, $queryRW4);
+// RW5
+$queryRW5 = " select count(id_warga) as totalRW5 from warga where rw_warga = 5; ";
+$dataRW5 = mysqli_query($db_connection, $queryRW5);
+// RW6
+$queryRW6 = " select count(id_warga) as totalRW6 from warga where rw_warga = 6; ";
+$dataRW6 = mysqli_query($db_connection, $queryRW6);
+// RW7
+$queryRW7 = " select count(id_warga) as totalRW7 from warga where rw_warga = 7; ";
+$dataRW7 = mysqli_query($db_connection, $queryRW7);
+// RW8
+$queryRW8 = " select count(id_warga) as totalRW8 from warga where rw_warga = 8; ";
+$dataRW8 = mysqli_query($db_connection, $queryRW8);
+// RW9
+$queryRW9 = " select count(id_warga) as totalRW9 from warga where rw_warga = 9; ";
+$dataRW9 = mysqli_query($db_connection, $queryRW9);
+// RW10
+$queryRW10 = " select count(id_warga) as totalRW10 from warga where rw_warga = 10; ";
+$dataRW10 = mysqli_query($db_connection, $queryRW10);
+// RW11
+$queryRW11 = " select count(id_warga) as totalRW11 from warga where rw_warga = 11; ";
+$dataRW11 = mysqli_query($db_connection, $queryRW11);
+// RW12
+$queryRW12 = " select count(id_warga) as totalRW12 from warga where rw_warga = 12; ";
+$dataRW12 = mysqli_query($db_connection, $queryRW12);
+// RW13
+$queryRW13 = " select count(id_warga) as totalRW13 from warga where rw_warga = 13; ";
+$dataRW13 = mysqli_query($db_connection, $queryRW13);
+// RW14
+$queryRW14 = " select count(id_warga) as totalRW14 from warga where rw_warga = 14; ";
+$dataRW14 = mysqli_query($db_connection, $queryRW14);
+// RW15
+$queryRW15 = " select count(id_warga) as totalRW15 from warga where rw_warga = 15; ";
+$dataRW15 = mysqli_query($db_connection, $queryRW15);
+// RW END
+// FORW1
+foreach ($dataRW1 as $data):
+    ?>
+    <p><?php echo $data['totalRW1'] ?></p>
+    </div>
+<?php endforeach ?>
 
+<!-- FORW2 -->
+<?php
+foreach ($dataRW2 as $data):
+    ?>
+    <p><?php echo $data['totalRW2'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW3 -->
+<?php
+foreach ($dataRW3 as $data):
+    ?>
+    <p><?php echo $data['totalRW3'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW4 -->
+<?php
+foreach ($dataRW4 as $data):
+    ?>
+    <p><?php echo $data['totalRW4'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW5 -->
+<?php
+foreach ($dataRW5 as $data):
+    ?>
+    <p><?php echo $data['totalRW5'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW6 -->
+<?php
+foreach ($dataRW6 as $data):
+    ?>
+    <p><?php echo $data['totalRW6'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW7 -->
+<?php
+foreach ($dataRW7 as $data):
+    ?>
+    <p><?php echo $data['totalRW7'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW8 -->
+<?php
+foreach ($dataRW8 as $data):
+    ?>
+    <p><?php echo $data['totalRW8'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW9 -->
+<?php
+foreach ($dataRW9 as $data):
+    ?>
+    <p><?php echo $data['totalRW9'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW10 -->
+<?php
+foreach ($dataRW10 as $data):
+    ?>
+    <p><?php echo $data['totalRW10'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW11 -->
+<?php
+foreach ($dataRW11 as $data):
+    ?>
+    <p><?php echo $data['totalRW11'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW12 -->
+<?php
+foreach ($dataRW12 as $data):
+    ?>
+    <p><?php echo $data['totalRW12'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW13 -->
+<?php
+foreach ($dataRW13 as $data):
+    ?>
+    <p><?php echo $data['totalRW13'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW14 -->
+<?php
+foreach ($dataRW14 as $data):
+    ?>
+    <p><?php echo $data['totalRW14'] ?></p>
+    </div>
+<?php endforeach ?>
+<!-- FORW15 -->
+<?php
+foreach ($dataRW15 as $data):
+    ?>
+    <p><?php echo $data['totalRW15'] ?></p>
+    </div>
+<?php endforeach ?>
 
 
 </body>
 
 <script>
     // 
-    const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+    const xValues = ["RW 1", "RW 2", "RW 3", "RW 4", "RW 5", "RW 6", "RW 7", "RW 8", "RW 9", "RW 10", "RW 11", "RW 12", "RW 13", "RW 14", "RW 15"];
     const yValues = [55, 49, 44, 24, 15];
     const barColors = ["red", "green", "blue", "orange", "brown"];
 
