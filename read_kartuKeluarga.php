4891715d9be94b9f2ce4514f18c95be34de223ea
adriswara <chevellerz454@yahoo.com>
2024-08-19 14:59:21 +0700

    tinggal :dinding, penerangan, air, sumber masak, kepemilikan rumah, asal info, 2biji 7 total

--- read_kartuKeluarga.php
+++ read_kartuKeluarga.php
@@ -82,36 +82,36 @@ if (!isset($_SESSION['login'])) {
           <td class="col-1"><?php echo $data['jumlah_anak_laki_hidup'] ?></td>
           <td class="col-1"><?php echo $data['jumlah_anak_perempuan_hidup'] ?></td>
           <td class="col-1"><?php echo $data['jumlah_anak_diinginkan'] ?></td>
-          <td class="col-1"><?php echo $data['status_ibadah_rutin_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['kepemilikan_akte_nikah_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_konflik_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_sumber_penghasilan_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_kelayakan_makanan_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_tabungan_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_sakit_disabilitas_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_penggunaan_sosial_media_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_waktu_interaksi_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_interaksi_pasutri'] ?></td>
-          <td class="col-1"><?php echo $data['status_berkreasi_luarrumah_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_ikut_gotongroyong_rt_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_balita_ikut_posyandu_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_balita_ikut_binkel_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_remaja_ikut_binkel_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_remaja_ikut_pusatinformasi_konseling_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_lansia_ikut_binkel_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_ibadah_rutin_keluarga'] == 1) ? 'Ya' : (($data['status_ibadah_rutin_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_ibadah_rutin_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['kepemilikan_akte_nikah_keluarga'] == 1) ? 'Ya' : (($data['kepemilikan_akte_nikah_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['kepemilikan_akte_nikah_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_konflik_keluarga'] == 1) ? 'Ya' : (($data['status_konflik_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_konflik_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_sumber_penghasilan_keluarga'] == 1) ? 'Ya' : (($data['status_sumber_penghasilan_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_sumber_penghasilan_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_kelayakan_makanan_keluarga'] == 1) ? 'Ya' : (($data['status_kelayakan_makanan_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_kelayakan_makanan_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_tabungan_keluarga'] == 1) ? 'Ya' : (($data['status_tabungan_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_tabungan_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_sakit_disabilitas_keluarga'] == 1) ? 'Ya' : (($data['status_sakit_disabilitas_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_sakit_disabilitas_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_penggunaan_sosial_media_keluarga'] == 1) ? 'Ya' : (($data['status_penggunaan_sosial_media_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_penggunaan_sosial_media_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_waktu_interaksi_keluarga'] == 1) ? 'Ya' : (($data['status_waktu_interaksi_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_waktu_interaksi_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_interaksi_pasutri'] == 1) ? 'Ya' : (($data['status_interaksi_pasutri'] == 0) ? 'Tidak' : 'Data Error'); $data['status_interaksi_pasutri'] ?></td>
+          <td class="col-1"><?php echo ($data['status_berkreasi_luarrumah_keluarga'] == 1) ? 'Ya' : (($data['status_berkreasi_luarrumah_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_berkreasi_luarrumah_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_ikut_gotongroyong_rt_keluarga'] == 1) ? 'Ya' : (($data['status_ikut_gotongroyong_rt_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_ikut_gotongroyong_rt_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_balita_ikut_posyandu_keluarga'] == 1) ? 'Ya' : (($data['status_balita_ikut_posyandu_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_balita_ikut_posyandu_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_balita_ikut_binkel_keluarga'] == 1) ? 'Ya' : (($data['status_balita_ikut_binkel_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_balita_ikut_binkel_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_remaja_ikut_binkel_keluarga'] == 1) ? 'Ya' : (($data['status_remaja_ikut_binkel_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_remaja_ikut_binkel_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_remaja_ikut_pusatinformasi_konseling_keluarga'] == 1) ? 'Ya' : (($data['status_remaja_ikut_pusatinformasi_konseling_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_remaja_ikut_pusatinformasi_konseling_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_lansia_ikut_binkel_keluarga'] == 1) ? 'Ya' : (($data['status_lansia_ikut_binkel_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_lansia_ikut_binkel_keluarga'] ?></td>
           <td class="col-1"><?php echo ($data['jenis_atap_rumah_keluarga'] == 1) ? 'Beton' : (($data['jenis_atap_rumah_keluarga'] == 2) ? 'Genteng' : (($data['jenis_atap_rumah_keluarga'] == 3) ? 'Asbes' : ((($data['jenis_atap_rumah_keluarga'] == 4) ? 'Kayu/Sirap' : ((($data['jenis_atap_rumah_keluarga'] == 5) ? 'Bambu' : ((($data['jenis_atap_rumah_keluarga'] == 6) ? 'Jerami/Ijuk/Daun-daunan' : ((($data['jenis_atap_rumah_keluarga'] == 7) ? 'Lainnya' : 'Data Error'))))))))));?></td>
           <td class="col-1"><?php echo $data['jenis_dinding_keluarga'] ?></td>
           <td class="col-1"><?php echo ($data['jenis_lantai_keluarga'] == 1) ? 'Keramik/Marmer/Ubin/Tegel' : (($data['jenis_lantai_keluarga'] == 2) ? 'Semen' : (($data['jenis_lantai_keluarga'] == 3) ? 'Kayu/Papan' : ((($data['jenis_lantai_keluarga'] == 4) ? 'Bambu' : ((($data['jenis_lantai_keluarga'] == 5) ? 'Tanah' : ((($data['jenis_lantai_keluarga'] == 6) ? 'Lainnya' : 'Data Error'))))))));?></td>
           <td class="col-1"><?php echo $data['sumber_penerangan_keluarga'] ?></td>
           <td class="col-1"><?php echo $data['sumber_air_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_fasilitas_bab_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_fasilitas_bab_keluarga'] == 1) ? 'Ya' : (($data['status_fasilitas_bab_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_fasilitas_bab_keluarga'] ?></td>
           <td class="col-1"><?php echo $data['luas_rumah_mPersegi_keluarga'] ?></td>
           <td class="col-1"><?php echo $data['total_orang_menetap_dirumah_keluarga'] ?></td>
           <td class="col-1"><?php echo $data['bahan_bakar_utama_memasak_keluarga'] ?></td>
           <td class="col-1"><?php echo $data['kepemilikan_rumah_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_perolehan_info_kb_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_perolehan_info_kb_keluarga'] == 1) ? 'Ya' : (($data['status_perolehan_info_kb_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_perolehan_info_kb_keluarga'] ?></td>
           <td class="col-1"><?php echo $data['asal_perolehan_info_kb_keluarga'] ?></td>
-          <td class="col-1"><?php echo $data['status_petugas_asal_informasi_kb_keluarga'] ?></td>
+          <td class="col-1"><?php echo ($data['status_petugas_asal_informasi_kb_keluarga'] == 1) ? 'Ya' : (($data['status_petugas_asal_informasi_kb_keluarga'] == 0) ? 'Tidak' : 'Data Error'); $data['status_petugas_asal_informasi_kb_keluarga'] ?></td>
           <td class="col-1"><?php echo $data['asal_petugas_perolehan_info_kb_keluarga'] ?></td>
 
 
