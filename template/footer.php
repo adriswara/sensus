30bfde3ba4b6954b7fb0e6b849716f6960ba10a1
adriswara <chevellerz454@yahoo.com>
2024-08-20 11:55:18 +0700

    footer

--- template/footer.php
+++ template/footer.php
@@ -2,43 +2,33 @@
     <div class="container">
         <div class="row row-30">
             <div class="col-md-4 col-xl-5">
-                <div class="pr-xl-4"><a class="brand" href="index.html"><img class="brand-logo-light"
-                            src="images/agency/logo-inverse-140x37.png" alt="" width="140" height="37"
-                            srcset="images/agency/logo-retina-inverse-280x74.png 2x"></a>
-                    <p>We are an award-winning creative agency, dedicated to the best result in web design, promotion,
-                        business consulting, and marketing.</p>
+                <div class="pr-xl-4"><a class="brand" href="index.html"><img class="brand-logo-light" src="img\logoTransparent.png" alt="" width="140" height="37"></a>
+                    <p>Projek aplikasi sistem sensus oleh program kerja unggulan KKN kelompok 18 Mekarapitan </p>
                     <!-- Rights-->
                     <p class="rights"><span>©  </span><span
-                            class="copyright-year">2018</span><span> </span><span>Waves</span><span>. </span><span>All
-                            Rights Reserved.</span></p>
+                            class="copyright-year">2024</span><span> </span><span>KKNM Kelompok 18</span><span>. </span><span>Mekarapitan Universitas Langlangbuana</span></p>
+                            <br>
+                            <p>Instagram @mekarapitan</p>
                 </div>
             </div>
             <div class="col-md-4">
-                <h5>Contacts</h5>
-                <dl class="contact-list">
-                    <dt>Address:</dt>
-                    <dd>798 South Park Avenue, Jaipur, Raj</dd>
-                </dl>
-                <dl class="contact-list">
-                    <dt>email:</dt>
-                    <dd><a href="mailto:#">dkstudioin@gmail.com</a></dd>
-                </dl>
-                <dl class="contact-list">
-                    <dt>phones:</dt>
-                    <dd><a href="tel:#">https://karosearch.com</a> <span>or</span> <a
-                            href="tel:#">https://karosearch.com</a>
-                    </dd>
-                </dl>
+                
             </div>
             <div class="col-md-4 col-xl-3">
-                <h5>Links</h5>
-                <ul class="nav-list">
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">Contacts</a></li>
-                    <li><a href="#">Pricing</a></li>
-                </ul>
+            <h5>Contacts</h5>
+                <dl class="contact-list">
+                    <dt>Alamat:</dt>
+                    <dd>Jl. Laswi 131/262 Manggahang Baleendah</dd>
+                </dl>
+                <dl class="contact-list">
+                    <dt>Instagram:</dt>
+                    <dd><a href="mailto:#">Instagram @mekarapitan </a></dd>
+                </dl>
+                <dl class="contact-list">
+                    <dt>No Telefon:</dt>
+                    <dd><a href="tel:#">+628xxxxxxxx</a>
+                    </dd>
+                </dl>
             </div>
         </div>
     </div>
